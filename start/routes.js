75f4f666a7c709572d05cb391200a42802e320c3
James Maddox <james.maddox07@gmail.com>
2019-02-07 03:14:33 -0500

    completed register page

--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,8 @@ const Route = use('Route')
 Route.get('/', 'PageController.home');
 Route.get('/about', 'PageController.about');
 Route.get('/products', 'ProductController.index');
+Route.get('/products/new-arrivals', 'ProductController.newArrivals');
+
+// User authentication
+Route.get('/register', 'UserController.register');
+Route.get('/login', 'UserController.login');
